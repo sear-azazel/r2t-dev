0bc0bd1aafd2d7b8511f71feaca6d6790c432bce
iwamitsuRyota <iwamitsu@medicalnetwork.co.jp>
2019-03-29 16:41:18 +0900

    php学習のため。

--- index.php
+++ index.php
@@ -12,13 +12,13 @@
 				<img class = 'newappIcon' src='images/newapp-icon.png'>
 			</td>
 			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
+				<h1 id = "message"><?php echo "php学習するよ"; ?></h1>
 				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
 			</td>
 		</tr>
 		<tr>
 			<td>
-				<p>いま
+				<p>現在
 				<?php
 					date_default_timezone_set('Asia/Tokyo');
 					echo date("Y年m月d日H時i分s秒");
@@ -27,6 +27,23 @@
 			</td>
 			<td><a href="apps/app-a/">Application A page.</a></td>
 		</tr>
+		<tr>
+			<td>			
+				<?php
+					#変数を使ってみようか
+					echo 'シングルクォーテーションとダブルクォーテーションに注意が必要って感じなのね'
+					
+					$name = "岩満"
+					
+					echo "私の名前は$nameです(echoで表示)"
+					echo '<br>'
+					echo 'よろしくお願いします'
+					echo "ってか常にダブルクォーテーション使っとけば問題ないんじゃない説"
+					
+				?>
+			</td>
+		</tr>
+	
 	</table>
 </body>
 </html>
